a79304964abc234006b04e3707dd8042f03c1fbf
dre <draigan.lefebvre@gmail.com>
2024-01-25 21:18:17 -0500

    Add basic structure

---
+++ src/reading/ReadingMain.jsx
@@ -0,0 +1,44 @@
+import ShowWords from "./ShowWords";
+import "../css/reading.css";
+import ShowPictures from "./ShowPictures";
+import { useEffect, useState } from "react";
+
+const ReadingMain = () => {
+  const [currentWord, setCurrentWord] = useState("");
+  const [words, setwords] = useState([
+    "Dog",
+    "Cat",
+    "Tree",
+    "House",
+    "Book",
+    "Fork",
+    "Leaf",
+    "Star",
+    "Banana",
+    "Carrot",
+    "Toy",
+    "Train",
+    "Car",
+  ]);
+
+  function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  function changeWord() {
+    return setCurrentWord(getRandomWord());
+  }
+
+  return (
+    <div className="reading-main">
+      {currentWord}
+      <ShowWords />
+      <ShowPictures />
+      <div onClick={changeWord} style={{ backgroundColor: "black" }}>
+        change word
+      </div>
+    </div>
+  );
+};
+
+export default ReadingMain;
---
+++ src/reading/ShowPictures.jsx
@@ -0,0 +1,5 @@
+const ShowPictures = () => {
+  return <div>Hey from pictures</div>;
+};
+
+export default ShowPictures;
